9bf8991d9f7f7d9a49abd2defdf01304e1e0b4f7
NikV34 <25172610+NikV34@users.noreply.github.com>
2020-09-18 22:14:08 +0200

    frame refactoring

--- src/components/card-container/card-container.js
+++ src/components/card-container/card-container.js
@@ -1,16 +1,9 @@
 import React, {Component} from 'react';
 
 import './card-container.css';
-import frameDefault from './img/frame-default.svg';
-import frameDefaultHover from './img/frame-default-hover.svg';
-import frameSelected from './img/frame-selected.svg';
-import frameSelectedHover from './img/frame-selected-hover.svg';
-import frameDisabled from './img/frame-disabled.svg';
-import cat from './img/background-cat.png';
-import catDisabled from './img/background-cat-disabled.png';
-import cardBackground from './img/card-background.svg';
 
 import Card from '../card';
+import Frame from '../frame';
 
 class CardContainer extends Component {
 
@@ -18,50 +11,8 @@ class CardContainer extends Component {
     noHover: false
   }
 
-  //background style combination
   render() {
     const { content, onToggleSelection, onToggleHover } = this.props;
-
-    const backgroundLink = {
-      frameDefault: `url(${frameDefault})`,
-      frameDefaultHover: `url(${frameDefaultHover})`,
-      frameSelected: `url(${frameSelected})`,
-      frameSelectedHover: `url(${frameSelectedHover})`,
-      frameDisabled: `url(${frameDisabled})`,
-      cat: `no-repeat url(${cat}) 0% 100%`,
-      catDisabled: `no-repeat url(${catDisabled}) 0% 100%`,
-      solidGrey: `url(${cardBackground})`
-    }
-
-    let background = [backgroundLink.cat, backgroundLink.solidGrey]
-
-    switch (content.status) {
-      case "default":
-        if (content.hover) {
-          background.unshift(backgroundLink.frameDefaultHover);
-        } else {
-          background.unshift(backgroundLink.frameDefault);
-        }
-        break;
-      case "selected":
-        if (content.hover) {
-          background.unshift(backgroundLink.frameSelectedHover);
-        } else {
-          background.unshift(backgroundLink.frameSelected);
-        }
-        break;
-      case "disabled":
-        background.splice(0, 1, backgroundLink.catDisabled);
-        background.unshift(backgroundLink.frameDisabled);
-        break;
-      default:
-        background.unshift(backgroundLink.frameDefault);
-        break;
-    }
-
-    const style = {
-      background: background
-    }
 
     const onSelect = () => {
       onToggleSelection();
@@ -78,19 +29,19 @@ class CardContainer extends Component {
     if (content.status !== 'disabled') {
       return (
         <div
-          style={style}
           className={`card-container ${this.state.noHover ? "no-hover" : ""}`}
           onClick={() => {onSelect()} }
           onMouseEnter={onToggleHover}
-          onMouseLeave={this.state.noHover ? onMouseLeave : onToggleHover}
-        >
-          <Card content={content} noHover={this.state.noHover}/>
+          onMouseLeave={this.state.noHover ? onMouseLeave : onToggleHover}>
+            <Card content={content} noHover={this.state.noHover}/>
+            <Frame status={content.status} hover={content.hover}/>
         </div>
       )
     } else {
         return (
-          <div style={style} className="card-container">
+          <div className="card-container">
             <Card content={content}/>
+            <Frame status={content.status} hover={content.hover}/>
           </div>
         )
       }
